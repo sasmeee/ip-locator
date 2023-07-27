848ad98796e5425c5cb5e5f250edf5f304437c16
BlackAmda <sl.amdakolla@gmail.com>
2023-07-27 17:19:14 +0530

    feat: provide CLI command
    CLI Command: "getip" with custom script name and usage info for easy geodata retrieval from IP address via terminal.

---
+++ dist/cli.js
@@ -0,0 +1,25 @@
+const yargs = require("yargs");
+const { getGeoData } = require("../index");
+
+yargs
+    .scriptName("getip")
+    .usage(`Usage: getip geodata <ip_address>`) // Educational purpose only :) - BlackAmda
+    .command(
+        'geodata <ip_address>',
+        'Fetch geo data for the provided IP address',
+        (yargs) => {
+            yargs.positional('ip_address', {
+                describe: 'The IP address to fetch geo data for',
+                type: 'string',
+            });
+        },
+        async (argv) => {
+            const ip = argv.ip_address;
+            try {
+                const data = await getGeoData(ip);
+                console.log(data);
+            } catch (error) {
+                console.error('Error fetching geolocation data:', error.message);
+            }
+        }
+    ).argv;
